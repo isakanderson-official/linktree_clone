a41f157b082432c01aef32bca82d2a05d62542c9
Isak Anderson <isak@isakanderson.com>
2023-03-03 17:11:31 -0800

    Header Bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import CustomButton from '../components/CustomButton';
+import DotsSvg from '../components/DotsSvg';
 
 const HomePage = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <>
+      {/* Header Top Bar */}
+      {scrollTop > 45 && (
+        <div className='fixed w-[calc(100%-24px)] left-0 right-0 mx-auto my-[8px] py-[8px] px-[12px] z-10 top-0 flex flex-row-reverse items-center justify-between bg-[#ffffff80] rounded-[72px] backdrop-blur-[10px] border-[1px] border-[#ebeef1]'>
+          {/* Botton */}
+          <button className='w-[40px] h-[40px] bg-[#f0f0f0] dark:bg-black hover:bg-[#ebebeb] transition-all duration-[150ms] rounded-full flex items-center justify-center border-[1px] border-[#e3e3e3] dark:border-black'>
+            <DotsSvg className='align-middle dark:text-white' />
+          </button>
+          <div>
+            <p className='font-semibold'>@isakanderson {scrollTop}</p>
+          </div>
+          <div className='max-w-[44px] max-h-[44px]'>
+            <img
+              src={
+                'https://d1fdloi71mui9q.cloudfront.net/aobYU4TDSXOOdof3MYND_9TvbS17m6tobN8lY'
+              }
+              className={'object-contain block rounded-full'}
+            />
+          </div>
+        </div>
+      )}
+      {/* End Top Bar */}
+
       {/* Base Styles */}
       <div className='flex flex-col items-center overflow-x-hidden w-full h-full min-h-[100lvh] antialiased'>
         {/* Container */}
@@ -38,7 +75,6 @@ const HomePage = () => {
               <CustomButton>My Air Fryer!!</CustomButton>
             </div>
           </div>
-          {/* <div className=' w-[96px] h-[96px] rounded-full overflow-hidden mx-[12px]'></div> */}
         </div>
       </div>
     </>

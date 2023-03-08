253052714717e26bcf65eb6c71b4194a3401d47a
isakanderson-official <94191077+isakanderson-official@users.noreply.github.com>
2023-03-08 08:06:17 -0800

    Header changes

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,7 +2,7 @@ import { Transition } from '@headlessui/react';
 import React, { Fragment } from 'react';
 import DotsSvg from '../DotsSvg';
 
-const Button = () => {
+const Button = ({ isDark }) => {
   return (
     <button className='w-[40px] h-[40px] bg-[#f0f0f0] dark:bg-black hover:bg-[#ebebeb] transition-all duration-[150ms] rounded-full flex items-center justify-center border-[1px] border-[#e3e3e3] dark:border-black'>
       <DotsSvg className='align-middle dark:text-white' />
@@ -39,7 +39,8 @@ const Header = ({ isShowing }) => {
           </div>
         </div>
       </Transition>
-      <div className='fixed w-[calc(100%-24px)] left-0 right-0 mx-auto my-[8px] py-[8px] px-[12px] z-10 top-0 flex flex-row-reverse items-center justify-between bg-[#ffffff80] rounded-[72px] backdrop-blur-[10px] border-[1px] border-[#ebeef1]'>
+      <div className='fixed w-[calc(100%-24px)] left-0 right-0 mx-auto my-[8px] py-[8px] px-[12px] z-10 top-0 flex flex-row items-center justify-between rounded-[72px] border-[1px] border-opacity-0'>
+        <div className='h-[44px] w-[44px] opacity-0' />
         <Button />
       </div>
     </>

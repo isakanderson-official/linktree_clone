2b926361322a8d9aeff8fee5e7c8bca5bc6e5873
Isak Anderson <isak@isakanderson.com>
2023-03-14 16:52:04 -0700

    cool header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,46 +2,53 @@ import { Transition } from '@headlessui/react';
 import React, { Fragment } from 'react';
 import DotsSvg from '../DotsSvg';
 
-const Button = () => {
+const Button = ({ isDark }) => {
   return (
-    <button className='w-[40px] h-[40px] bg-[#f0f0f0] dark:bg-black hover:bg-[#ebebeb] transition-all duration-[150ms] rounded-full flex items-center justify-center border-[1px] border-[#e3e3e3] dark:border-black'>
-      <DotsSvg className='align-middle dark:text-white' />
+    <button className='w-[40px] h-[40px] bg-[#f0f0f0] hover:bg-[#ebebeb] transition-all duration-[150ms] rounded-full flex items-center justify-center border-[1px] border-[#e3e3e3]'>
+      <DotsSvg className='align-middle text-black' />
     </button>
   );
 };
 
 const Header = ({ isShowing }) => {
+  const activeHeaderStyles = isShowing
+    ? 'bg-[#ffffff80] backdrop-blur-[10px] border-[#ebeef1]'
+    : ' border-[#0000ff00]';
+
+  const activeHeaderItemStyles = isShowing ? '' : 'opacity-0';
+
   return (
     <>
-      <Transition
+      {/* <Transition
         as={Fragment}
         show={isShowing}
-        enter='transition-opacity  duration-150'
+        enter='transition-opacity duration-150'
         enterFrom='opacity-0'
         enterTo='opacity-100'
-        leave='transition-opacity  duration-150'
+        leave='transition-opacity duration-150'
         leaveFrom='opacity-100'
         leaveTo='opacity-0'
+      > */}
+      <div
+        className={`fixed w-[calc(100%-24px)] left-0 right-0 mx-auto my-[8px] py-[8px] px-[12px] z-10 top-0 flex flex-row-reverse items-center justify-between rounded-[72px] border-[1px] ${activeHeaderStyles}`}
       >
-        <div className='fixed w-[calc(100%-24px)] left-0 right-0 mx-auto my-[8px] py-[8px] px-[12px] z-10 top-0 flex flex-row-reverse items-center justify-between bg-[#ffffff80] rounded-[72px] backdrop-blur-[10px] border-[1px] border-[#ebeef1]'>
-          {/* Botton */}
-          <Button />
-          <div>
-            <p className='font-semibold'>@isakanderson</p>
-          </div>
-          <div className='max-w-[44px] max-h-[44px]'>
-            <img
-              src={
-                'https://d1fdloi71mui9q.cloudfront.net/aobYU4TDSXOOdof3MYND_9TvbS17m6tobN8lY'
-              }
-              className={'object-contain block rounded-full'}
-            />
-          </div>
-        </div>
-      </Transition>
-      <div className='fixed w-[calc(100%-24px)] left-0 right-0 mx-auto my-[8px] py-[8px] px-[12px] z-10 top-0 flex flex-row-reverse items-center justify-between bg-[#ffffff80] rounded-[72px] backdrop-blur-[10px] border-[1px] border-[#ebeef1]'>
+        {/* Botton */}
         <Button />
+        <div>
+          <p className={`font-semibold ${activeHeaderItemStyles}`}>
+            @isakanderson
+          </p>
+        </div>
+        <div className='max-w-[44px] max-h-[44px]'>
+          <img
+            src={
+              'https://d1fdloi71mui9q.cloudfront.net/aobYU4TDSXOOdof3MYND_9TvbS17m6tobN8lY'
+            }
+            className={`object-contain block rounded-full ${activeHeaderItemStyles} `}
+          />
+        </div>
       </div>
+      {/* </Transition> */}
     </>
   );
 };

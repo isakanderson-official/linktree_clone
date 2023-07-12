6e9a3f8fa5d9a2e651fb0997d00a2105d591b938
Isak Anderson <isak@isakanderson.com>
2023-07-11 17:02:03 -0700

    links

---
+++ src/links.js
@@ -0,0 +1,37 @@
+const links = [
+  {
+    title: 'My Recommended Tech',
+    link: 'https://amzn.to/3rIkdgK',
+    tags: ['Amazon', 'Favorite Products'],
+    description:
+      'These are all the products that I have either owned, or support fully. ',
+    iconType: 'emoji',
+    icon: '⚙️',
+  },
+  {
+    title: 'Instagram',
+    link: 'http://www.instagram.com/videotechdude',
+    tags: ['Social'],
+    description: '',
+    iconType: 'emoji',
+    icon: '📱',
+  },
+  {
+    title: 'Youtube',
+    link: 'http://www.youtube.com/videotechdude',
+    tags: ['Youtube'],
+    description: '',
+    iconType: 'emoji',
+    icon: '🎥',
+  },
+  {
+    title: 'My Air Fryer!!',
+    link: 'https://amzn.to/3mLHz3V',
+    tags: ['Amazon', 'Product'],
+    description: '',
+    iconType: 'emoji',
+    icon: '💰',
+  },
+];
+
+export default links;
